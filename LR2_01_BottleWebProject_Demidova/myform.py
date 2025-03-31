342ca45e0559fc1bd005d5efb6c6b7f5d5e95280
Svetik <sveta_dem06@mail.ru>
2025-03-31 20:42:01 +0300

    Улучшена проверка почты

--- LR2_01_BottleWebProject_Demidova/myform.py
+++ LR2_01_BottleWebProject_Demidova/myform.py
@@ -13,7 +13,8 @@ def my_form():
         return "All fields are required. Please fill out the form."
 
     # Проверка формата email
-    email_pattern = r'^[a-zA-Z0-9_-]+@[a-zA-Z]+\.[a-zA-Z]+$'
+    email_pattern = r"^(?!\.)(?!.*\.\.)[a-zA-Z0-9_.-]{2,64}@(?=.{1,255}$)[a-zA-Z0-9-]+\.[a-zA-Z]{2,}$"
+
     if not re.fullmatch(email_pattern, email_address):
         return "Invalid email format. Please enter a valid email."
 
